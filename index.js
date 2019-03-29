46e6af7da9a0298a1c7d046682229265bc1c878c
karen-mtz <dkareen.mtz@gmail.com>
2019-03-29 00:15:11 -0600

    en index.js se uso el modulo file system,  se guardó  en una variable la ruta del archivo a leer, esta ruta se obtuvo con process.argv  a partir de lo que se ingresa en la consola esto regresa un array y la ruta del archivo para leer se encuentra en el indice 2 del array,  posteriormente se uso la funcion readfile y se le dieron 3 argumentos: la ruta del archivo y el convertidor a string de ese archivo, y un callback

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 module.exports = () => {
   // ...
 };
+
+let fs = require('fs'); //Traer modulo de file system
+let fileRoute = process.argv[2] /*guardar en una variable la ruta que ingresan en consola para pasarla como parametro 
+a la función de leer archivo e imprimirlo en la consola*/
+
+fs.readFile(fileRoute, 'utf8' , (err, content) => {
+    if(err) {
+        return console.log(err)
+    } //agregar qué hacer en caso de error
+    console.log(content)
+});
+/*función para leer archivo de forma asíncrona (no usa Sync): readFile tiene 3
+argumentos: 1 = la ruta del archivo 2 = el formato de codificación de caracteres utf8
+3 = callback que procesa el resultado y se invoca al terminar la lectura del archivo */
